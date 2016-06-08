6d602e952ce0f8ab2c38a827ddfa2b25a20bcbd3
Owen Yang <usay@outlook.com>
2016-06-08 21:53:12 +0800

    update home page

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,10 +1,13 @@
 /* @flow */
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
+import classnames from 'classnames/bind'
 
 import { increment, doubleAsync } from '../../redux/modules/counter'
 import DuckImage from './Duck.jpg'
 import classes from './HomeView.scss'
+
+const cx = classnames.bind(classes)
 
 // We can use Flow (http://flowtype.org/) to type our component's props
 // and state. For convenience we've included both regular propTypes and
@@ -32,27 +35,19 @@ export class HomeView extends React.Component {
 
   render () {
     return (
-      <div className='container text-center'>
-        <div className='row'>
-          <div className='col-xs-2 col-xs-offset-5'>
-            <img className={classes.duck}
-              src={DuckImage}
-              alt='This is a duck, because Redux.' />
+      <div className={cx('container', 'home')}>
+        <div className='hero'>
+          <h2 className='hero__head'>AiChi Admin Dashboard</h2>
+          <p className={cx('hero__text')}>Use our serverless platform to operate your data in minutes.</p>
+          <div className={cx('hero__btn--container')}>
+            <button className='btn btn-primary' onClick={this.props.increment}>GET STARTED</button>
+            {' '}
+            <button className='btn btn-default' onClick={this.props.doubleAsync}>
+              Double (Async)
+            </button>
+            <div className={cx('counter')}>{this.props.counter}</div>
           </div>
         </div>
-        <h1>Welcome to the React Redux Starter Kit</h1>
-        <h2>
-          Sample Counter:
-          {' '}
-          <span className={classes['counter--green']}>{this.props.counter}</span>
-        </h2>
-        <button className='btn btn-default' onClick={this.props.increment}>
-          Increment
-        </button>
-        {' '}
-        <button className='btn btn-default' onClick={this.props.doubleAsync}>
-          Double (Async)
-        </button>
       </div>
     )
   }
